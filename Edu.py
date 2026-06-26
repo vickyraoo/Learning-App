85f0c9b4a97d255ab6effcd0e190d4a75eb4b881
Punit <159620341+vickyraoo@users.noreply.github.com>
2026-06-26 16:33:50 +0530

    Update Edu.py

--- Edu.py
+++ Edu.py
@@ -378,7 +378,7 @@ if __name__ == "__main__":
             host="localhost",
             database="flashcards_app",
             user="postgres",
-            password="1212",
+            password="password",
             port="5432"
         )
         create_tables(conn)
